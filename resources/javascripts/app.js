b8fd961784af30ff1f96eaae226d2c60c0b5f0fb
Brant Wladichuk <brant@interwebular.net>
2015-12-08 17:37:03 -0800

    Fix scrolling

--- resources/javascripts/app.js
+++ resources/javascripts/app.js
@@ -22,27 +22,27 @@ $(function(){
 });
 
 
-if (window.addEventListener) window.addEventListener('DOMMouseScroll', wheel, false);
-window.onmousewheel = document.onmousewheel = wheel;
-
-function wheel(event) {
-    var delta = 0;
-    if (event.wheelDelta) delta = event.wheelDelta / 120;
-    else if (event.detail) delta = -event.detail / 3;
-
-    handle(delta);
-    if (event.preventDefault) event.preventDefault();
-    event.returnValue = false;
-}
-
-function handle(delta) {
-    var time = 500; // delay time
-    var distance = 200; // delta point
-
-    $('html, body').stop().animate({
-        scrollTop: $(window).scrollTop() - (distance * delta)
-    }, time );
-}
+// if (window.addEventListener) window.addEventListener('DOMMouseScroll', wheel, false);
+// window.onmousewheel = document.onmousewheel = wheel;
+//
+// function wheel(event) {
+//     var delta = 0;
+//     if (event.wheelDelta) delta = event.wheelDelta / 120;
+//     else if (event.detail) delta = -event.detail / 3;
+//
+//     handle(delta);
+//     if (event.preventDefault) event.preventDefault();
+//     event.returnValue = false;
+// }
+//
+// function handle(delta) {
+//     var time = 500; // delay time
+//     var distance = 200; // delta point
+//
+//     $('html, body').stop().animate({
+//         scrollTop: $(window).scrollTop() - (distance * delta)
+//     }, time );
+// }
 
 // SMOOTH SCROLL TO
 $('a[href*=#]:not([href=#])').click(function () {
